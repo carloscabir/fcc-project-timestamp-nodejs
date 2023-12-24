72e4a56fa43fa09c39b769e140fdc0cb7250bd2c
Carlos Cabi <carlangascamachobrigido@gmail.com>
2023-12-24 13:37:50 -0500

    Project done at `index.js`

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# Timestamp Microservice
+# Timestamp Microservice Project 
 
 This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+
+> Project made by _Carlos Cabi_!
+>
+> Take a look to this little project for my _Back End Development and APIs Projects_ **Certification** by **FreeCodeCamp**! 
+
+
+## Get Started
+- Clone repository
+```
+git clone "url"
+```
+
+- Move to project 
+```cmd
+cd project/name
+```
+
+- Install dependencies
+```cmd
+npm i
+```
+
+- Run project!!
+```
+npm start
+```
--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+const dotenv = require('dotenv')
+dotenv.config()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -12,6 +14,8 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(express.json())
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -24,7 +28,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  let { date } = req.params
+  const isYMDRegex = /\d\d\d\d-\d\d-\d\d/.test(date)
 
+  if (!isYMDRegex) date = parseInt(date)
+
+  const ISODate = new Date(date)
+
+  const response = {
+    unix: new Date(ISODate).getTime(),
+    utc: new Date(ISODate).toUTCString()
+  }
+  
+  res.json(response)
+ })
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
